651655fc0055f7563ced298bffaf276ceb25d9ce
areeja48 <honor12pakistan@gmail.com>
2025-03-13 06:33:56 +0500

    update

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,5 +1,7 @@
-import NextAuth from 'next-auth'
-import { JWT } from 'next-auth/jwt'
+// src/types/next-auth.d.ts
+
+import 'next-auth'
+import 'next-auth/jwt'
 
 declare module 'next-auth' {
   interface Session {
