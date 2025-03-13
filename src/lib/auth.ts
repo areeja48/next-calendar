8f8490abb33ea8c14b02b30b5f34c5583c3c1829
areeja48 <honor12pakistan@gmail.com>
2025-03-13 06:06:38 +0500

    update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -39,7 +39,7 @@ export const authOptions: AuthOptions = {
       }
       return session
     },
-    async redirect({ url, baseUrl }) {
+    async redirect({ baseUrl }) {
       return `${baseUrl}/dashboard`
     },
   },
