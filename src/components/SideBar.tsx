396f18f66d6af7c398a3cb63d5629fffaa6c1eb2
areeja48 <honor12pakistan@gmail.com>
2025-03-22 20:32:54 +0500

    edit

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { CalendarDays, LayoutDashboard, PlusCircle } from 'lucide-react';
+import { LayoutDashboard, PlusCircle } from 'lucide-react';
 
 type SideBarProps = {
   onAddGoogleEventClick: () => void;
