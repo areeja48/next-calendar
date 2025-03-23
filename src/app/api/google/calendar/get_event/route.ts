23b452ed319f0aef55fd8d5b9078d2f77c1c1f96
areeja48 <honor12pakistan@gmail.com>
2025-03-23 21:49:54 +0500

    edit

--- src/app/api/google/calendar/get_event/route.ts
+++ src/app/api/google/calendar/get_event/route.ts
@@ -1,19 +1,17 @@
-// src/app/api/google/calendar/get_events/route.ts
 import { google } from 'googleapis';
-import getServerSession  from 'next-auth/next';
-import { authOptions } from '@/lib/auth';
-import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+import { NextResponse , NextRequest} from 'next/server';
 
-export async function GET() {
-  const session = await getServerSession(authOptions);
-
-  if (!session || !session.accessToken) {
+export async function GET(req: NextRequest) {
+  const token = await getToken({ req }); 
+  // Check if token or access token is missing
+  if (!token || !token.accessToken)  {
     return NextResponse.json({ error: 'Unauthorized - No access token' }, { status: 401 });
   }
 
   try {
     const oauth2Client = new google.auth.OAuth2();
-    oauth2Client.setCredentials({ access_token: session.accessToken });
+    oauth2Client.setCredentials({ access_token: token.accessToken });
 
     const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
 
