99b358b267928e7179a99dec02eef8ab2060309b
areeja48 <honor12pakistan@gmail.com>
2025-03-23 09:33:25 +0500

    ediy

--- src/app/api/google/calendar/create_event/route.ts
+++ src/app/api/google/calendar/create_event/route.ts
@@ -2,7 +2,7 @@
 import { google } from 'googleapis';
 
 
-import  getServerSession  from 'next-auth/next';
+import  getServerSession  from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import { NextResponse } from 'next/server';
 
